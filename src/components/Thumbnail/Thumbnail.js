09e448d54e0767755070b5050800ae2474937c78
Armaan Butt <armaan.r.butt@gmail.com>
2018-02-07 04:06:06 -0500

    Created NavBar, thumbnail, and homepage components
    Created NavBar, thumbnail, and homepage components

---
+++ src/components/Thumbnail/Thumbnail.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Thumbnail.css';
+
+export default class Thumbnail extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="thumbnail-container">
+                <img src={this.props.imageSrc}/>
+                <div>
+                    <div className="thumbnail-span-heading">Heading</div>
+                    <div className="thumbnail-span-subheading">Subheading</div>
+                </div>
+            </div>
+        );
+    }
+}
