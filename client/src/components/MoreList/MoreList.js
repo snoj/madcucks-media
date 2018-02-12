560c0c57368682492127a6c17121618f32f3b1f8
Armaan Butt <armaan.r.butt@gmail.com>
2018-02-12 00:50:49 -0500

    Started work on the MoreList component
    Started work on the MoreList component. This component will show related episodes that the user can checkout on an episode page.

--- client/src/components/MoreList/MoreList.js
+++ client/src/components/MoreList/MoreList.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Router from 'react-router-dom';
 
 import './MoreList.css';
+
+const ListItem = ({ id, episodeInfo }) => (
+    <li>
+        <Link>
+            <div>
+                <div className="moreList-image-container">
+                    <img/>
+                    <p>date</p>
+                    <p>runtime</p>
+                </div>
+                <div className="moreList-description-container">
+                    <h1>{episodeInfo.title}</h1>
+                    <p>{episodeInfo.description}</p>
+                </div>
+            </div>
+        </Link>
+    </li>
+);
 
 class MoreList extends Component {
 
     render() {
+
+        let listenItemArray = [];
+
+        for(let i = 0; i < 7 && i < this.props.episodeArray.length; i++) {
+            listenItemArray.push(<ListItem id={this.props.episodeArray.length - i - 1} episodeInfo={this.props.episodeArray[i]} />);
+        }
+
         return(
-            <ul className="morelist-container">
-                
+            <ul className="moreList-container">
+                {/* {listenItemArray} */}
+                <div className="moreList-button">See More</div>
             </ul>
         );
     }
 }
-
-const ListItem = () => {
-    <li></li>
-};
 
 export default MoreList;
