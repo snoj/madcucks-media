ff75eda0ef9a9438b6922aed3f2b46bf6b604bd8
Armaan Butt <armaan.r.butt@gmail.com>
2018-02-16 01:48:00 -0500

    Added link to github at bottom of page :)

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -8,14 +8,14 @@ export default class NavBar extends Component {
         return(
             <header className="header-container">
                 <a className="logo">
-                    <h1>The Bestest Podcast Network in Existence<sup>v0.1.0</sup></h1>
-                    <h2>All the best podcasts in existence. Also this site cost $0.</h2>
+                    <h1>The Bestest Podcast Network in Existence <sup>v0.1.0</sup></h1>
+                    <h2>All the bestest podcasts in existence on site that cost $0.</h2>
                 </a>
                 <div className="navbar-button-container">
                     <ul className="navbar-button-list">
                         <Link to="/" className="navbar-button">Listen</Link>
-                        <Link to="/" className="navbar-button">Interact</Link>
-                        <Link to="/" className="navbar-button">Account</Link>
+                        <a href="https://www.patreon.com/thedickshow/posts" className="navbar-button">Patreon</a>
+                        <a href="https://thedickshow.bandcamp.com/releases" className="navbar-button">Album</a>
                     </ul>
                 </div>
             </header>
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
+import FaHeart from 'react-icons/lib/fa/heart'
+
 import NavBar from '../../components/NavBar/NavBar';
 import Homepage from '../Homepage/Homepage';
 import Show from '../../containers/Show/Show'; 
 import Episode from '../../components/Episode/Episode';
 import NotFound from '../NotFound/NotFound';
+
+import './App.css';
+import './Author.css'
+
+const Author = () => (
+  <div className="author-container">
+    <a href="https://github.com/ArmaanButt/madcucks-media/">
+      Made with <FaHeart/> by Armaan Butt
+    </a>
+    <div>
+      <p>8=D: 0xeaD2ACA81ACaEc540C99c15d01A4132ce8e5305E</p>
+    </div>
+  </div>
+);
 
 const App = () => (
   <Router onUpdate={() => window.scrollTo(0, 0)}>
@@ -18,6 +33,7 @@ const App = () => (
         <Route exact path="/shows/:show/:id" component={Episode}/>
         <Route path="*" component={NotFound}/>
       </Switch>
+      <Author/>
     </div>
   </Router>
 );
